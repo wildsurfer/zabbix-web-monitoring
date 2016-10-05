718e2c054464b40bcab404c129ba3ecbd48bded9
Ivan Kuznetsov <ivan@tep.io>
2016-10-05 17:35:22 +0300

    Initial import

---
+++ README.md
@@ -0,0 +1,8 @@
+# Automated creation of web scenarios in Zabbix
+
+This tool is build on top of jwilder/docker-gen project. It knows when new container is created and adds this container's URL to Zabbix Web tab. This way you don't have to maintain it manually. Works great when you have a lot of URL endpoints to monitor.
+
+Out of the box it will work if your containers will have `VIRTUAL_HOST` environment variable with url. To change the way it works you can adjust `endpoints.tmpl` to implement more complex logic. Check docker-gen documentation for more info.
+
+Check example folder to see how it will work with Zabbix. Just run `docker-compose up -d`. Then login to Zabbix and check Web tab, you should see some web scenarios there.
+
---
+++ zabbix-notify.sh
@@ -0,0 +1,119 @@
+#!/bin/sh
+
+# Login and get token
+
+TMPL_LOGIN=$(cat << EOF
+{
+    "jsonrpc": "2.0",
+    "method": "user.login",
+    "params": {
+        "user": "${ZBX_LOGIN:-Admin}",
+        "password": "${ZBX_PASSWORD:-zabbix}"
+    },
+    "id": 1,
+    "auth": null
+}
+EOF
+)
+
+TOKEN=$(curl -s -X POST -H 'Content-Type: application/json-rpc' -d "$TMPL_LOGIN" http://$ZBX_API_URL/api_jsonrpc.php | jq -r '.result')
+
+# Find out default node hostid
+
+TMPL_HOSTS=$(cat << EOF
+{
+    "jsonrpc": "2.0",
+    "method": "host.get",
+    "params": {
+        "output": [
+            "hostid"
+        ],
+        "filter": {
+            "host": "Zabbix server"
+        }
+    },
+    "id": 2,
+    "auth": "$TOKEN"
+}
+EOF
+)
+
+HOST_ID=$(curl -s -X POST -H 'Content-Type: application/json-rpc' -d "$TMPL_HOSTS" http://$ZBX_API_URL/api_jsonrpc.php | jq '.result[0]' | jq -r '.hostid')
+
+# Ensure host monitoring is enabled
+
+TMPL_MONIT_CHECK=$(cat << EOF
+{
+    "jsonrpc": "2.0",
+    "method": "host.update",
+    "params": {
+        "hostid": "$HOST_ID",
+        "status": 0
+    },
+    "auth": "$TOKEN",
+    "id": 1
+}
+EOF
+)
+
+curl -s -X POST -H 'Content-Type: application/json-rpc' -d "$TMPL_MONIT_CHECK" http://$ZBX_API_URL/api_jsonrpc.php | jq
+
+# Loop over all hosts in file
+
+while read EP; do
+
+# Check if web scenario already exist
+
+TMPL_VHOST_CHECK=$(cat << EOF
+{
+    "jsonrpc": "2.0",
+    "method": "httptest.get",
+    "params": {
+        "output":
+        [
+            "httptestid"
+        ],
+        "filter": {
+            "name": "$EP check"
+        }
+    },
+    "auth": "$TOKEN",
+    "id": 1
+}
+EOF
+)
+
+IF_VHOST_EXISTS=$(curl -s -X POST -H 'Content-Type: application/json-rpc' -d "$TMPL_VHOST_CHECK" http://$ZBX_API_URL/api_jsonrpc.php | jq ".result[0]" | jq -r ".httptestid")
+
+if [ $IF_VHOST_EXISTS = "null" ]
+then
+TMPL_VHOST_ADD=$(cat << EOF
+{
+    "jsonrpc": "2.0",
+    "method": "httptest.create",
+    "params": {
+        "name": "$EP check",
+        "hostid": "$HOST_ID",
+        "delay": "${ZBX_WEB_DELAY:-60}",
+        "timeout": "${ZBX_WEB_TIMEOUT:-15}",
+        "steps": [
+            {
+                "name": "Homepage",
+                "url": "http://$EP",
+                "status_codes": 200,
+                "no": 1
+            }
+        ]
+    },
+    "auth": "$TOKEN",
+    "id": 1
+}
+EOF
+)
+curl -s -X POST -H 'Content-Type: application/json-rpc' -d "$TMPL_VHOST_ADD" http://$ZBX_API_URL/api_jsonrpc.php
+else
+echo "'$EP' already exists"
+fi
+
+done < endpoints.list
+
